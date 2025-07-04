8e75cb1fae7b30b0270f9c8934adba29ce561de5
Alejandra <adalava@espol.edu.ec>
2025-07-04 17:38:18 -0500

    Feat: new endpoints

--- backend/clinic/urls.py
+++ backend/clinic/urls.py
@@ -2,10 +2,21 @@ from django.urls import include, path
 from .views import *
 
 urlpatterns = [
+    #Owners
     #GET
     path('get-owners', return_owners),
     path('owners/<int:owner_id>/pets', pets_by_owner),
-
     #POST
     path('owners/create', create_owner),
+    #DELETE
+    path('owners/delete/<int:owner_id>', delete_owner),
+    #PATCH
+    path('owners/update/<int:owner_id>', update_owner),
+
+    #Pets
+    path('pets/create', create_pet),
+    #DELETE
+    path('pets/delete/<int:pet_id>', delete_pet),
+    #PATCH
+    path('pets/update/<int:pet_id>', update_pet)
 ]
--- backend/clinic/views.py
+++ backend/clinic/views.py
@@ -1,10 +1,11 @@
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import *
 
 # Create your views here.
 
+#Owners
 def return_owners(request):
     owners = Owners.objects.all()
     serialized_owners=[]
@@ -46,7 +47,94 @@ def create_owner(request):
             name = data['name'],
             phone = data['phone']
         )
-    return JsonResponse({
-        'message' : 'Owner created successfully.',
-        "id" : owner.id
-    })
+        return JsonResponse({
+            "id" : owner.id,
+            "name" : owner.name,
+            "phone" : owner.phone
+        })
+    else:
+        return HttpResponse('Incorrect method.', status = 405)
+    
+def delete_owner(request, owner_id):
+    if request.method == 'DELETE':
+        owner = get_object_or_404(Owners, pk= owner_id)
+        owner.delete()
+        return HttpResponse(f'Owner with id {owner_id} was deleted.',status=200)
+    else:
+        return HttpResponse('Incorrect method.', status = 405)
+    
+def update_owner(request, owner_id):
+    if request.method == 'PATCH':
+        owner = get_object_or_404(Owners, pk = owner_id)
+        data = json.loads(request.body)
+        if(data.get('name')):
+            owner.name = data['name']
+        if(data.get('phone')):
+            owner.phone = data['phone']
+        owner.save()
+        return JsonResponse({
+            "id" : owner.id,
+            "name" : owner.name,
+            "phone" : owner.phone
+        })
+    else:
+        return HttpResponse('Incorrect method.', status = 405)
+
+#Pets
+
+def create_pet(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        try:
+            owner = Owners.objects.get(pk=data.get('owner_id'))
+        except Owners.DoesNotExist:
+            return JsonResponse(
+                {'error': 'Owner not found'},
+                status=404
+            )
+        pet = Pets.objects.create(
+            name = data['name'],
+            species = data.get('species','No species provided'),
+            age = data.get('age', 'No age provided.'),
+            owner_id = data.get('owner_id')
+        )
+        return JsonResponse({
+            "id" : pet.id,
+            "name" : pet.name,
+            "species": pet.species,
+            "age" : pet.age,
+            "owner_id" : pet.owner_id
+        })
+    else:
+        return HttpResponse('Incorrect method.', status = 405)
+
+def delete_pet(request, pet_id):
+    if request.method == 'DELETE':
+        pet = get_object_or_404(Owners, pk= pet_id)
+        pet.delete()
+        return HttpResponse(f'Pet with id {pet_id} was deleted.',status=200)
+    else:
+        return HttpResponse('Incorrect method.', status = 405)
+    
+def update_pet(request, pet_id):
+    if request.method == 'PATCH':
+        pet = get_object_or_404(Pets, pk = pet_id)
+        data = json.loads(request.body)
+        if(data.get('name')):
+            pet.name = data['name']
+        if(data.get('species')):
+            pet.species = data['species']
+        if(data.get('age')):
+            pet.age = data['age']
+        if('owner_id' in data):
+            pet.owner_id = data['owner_id']
+        pet.save()
+        return JsonResponse({
+            "id" : pet.id,
+            "name" : pet.name,
+            "species" : pet.species,
+            "age" : pet.age,
+            "owner_id" : pet.owner.id 
+        })
+    else:
+        return HttpResponse('Incorrect method.', status = 405)
